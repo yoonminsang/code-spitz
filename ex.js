77dc4ab453b753b949ae76b6ded44e511225af3e
yoonminsang <apdlyooapdl@naver.com>
2022-05-29 14:45:39 +0900

    dir 및 readme 작성

--- ex.js
+++ ex.js
@@ -51,3 +51,62 @@ function run() {
   shipping.setStrategy(fedex);
   console.log('Fedex Strategy: ' + shipping.calculate(package));
 }
+
+class QuackQuack {
+  do() {
+    this.복잡한함수1()
+    this.복잡한함수2()
+    console.log(‘꽥꽥꽥‘)
+  }
+  복잡한함수1 () {}
+  복잡한함수2 () {}
+}
+class QuackQuack2 {
+  do() {
+    this.복잡한함수1();
+    console.log(“꽥꽥꽥“);
+  }
+  복잡한함수1() {}
+}
+class NoFly {
+  do() {
+    console.log(“난 못날아“);
+  }
+}
+class DuckFactory {
+  static getDuck(type) {
+    if (type === “청둥오리“) {
+      return new Duck(new QuackQuack(), new NoFly());
+    } else if (type === “오리종류1”) {
+      new Duck(new QuackQuack2(), new Fly1());
+    } else if (type === “오리종류2") {
+      new Duck(new QuackQuack(), new Fly2());
+    } else if (type === “오리종류3”) {
+      new Duck(new QuackQuack(), new Fly3());
+    } else if (type === “오리종류4") {
+      new Duck(new QuackQuack(), new Fly4());
+    } else if (type === “오리종류5”) {
+      new Duck(new QuackQuack(), new Fly5());
+    }
+  }
+}
+class Duck {
+  quack
+  fly
+  constructor(quack, fly) {
+    this.quack = quack
+    this.fly = fly
+  }
+  quack() {
+    this.quack.do()
+  }
+  fly() {
+    this.fly.do()
+  }
+}
+class 청둥오리 extends Duck {
+}
+function run() {
+  const 오리1 = DuckFactory.getDuck(‘청둥오리’)
+  오리1.quack()
+}
